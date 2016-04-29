23f441e9ea34bc76e66c96ea52b6f9a30ce6290f
clejacquet <clem.jacquet@gmail.com>
2016-04-29 12:24:03 +0200

    first integrale functions

---
+++ src/integral/calc.py
@@ -0,0 +1,8 @@
+
+''' Retourne la dérivée de f (par morceaux)'''
+def derivate(f):
+    return
+
+''' Retourne le polynome de Lagrange associé à la fonction f sur [b, e] découpé en l tranches '''
+def pol_lagrange(f, b, e, l):
+    return
