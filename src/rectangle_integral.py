2b36534980ef2427da2dfd91d0adae4ab1ce5b4b
dchapotte <dylan.chapotte@hotmail.fr>
2016-05-10 12:01:49 +0200

    Update rectangle_integral.py

--- src/rectangle_integral.py
+++ src/rectangle_integral.py
@@ -7,12 +7,7 @@ def rectangle(f,a,b,n):
     return res
 
 """def f(x):
-<<<<<<< HEAD
     return x*x+5*x-2
 
 print(rectangle(f,0,10,0.01))"""
-=======
-    return x
 
-print(rectangle(f,0,1,1000))"""
->>>>>>> a74fa8eacc0ed72ef276729c3bf6622ef3b1dfd7
