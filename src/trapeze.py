c725161b566ad493040fabb7e047b4cb5ab34af7
LUCAS Remy <rlucas003@bucaneers.pedago.ipb.fr>
2016-05-10 11:44:17 +0200

    debug trapeze

--- src/trapeze.py
+++ src/trapeze.py
@@ -6,10 +6,4 @@ def trapeze(f ,a ,b ,n):
 	for i in np.arange(a,b,n):
 		res += ((f(i) + f(i+n))/2)*n
 
-	return n
-
-def fctn(x):
-
-	return x**2 + 5*x - 2
-
-print trapeze(fctn,0,10,0.01);
+	return res
