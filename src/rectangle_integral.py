2e0805fcd09de885949ac542f703880991e51fc9
CHAPOTTE Dylan <dchapotte@colts.pedago.ipb.fr>
2016-05-10 11:41:22 +0200

    test rectangle

--- src/rectangle_integral.py
+++ src/rectangle_integral.py
@@ -6,7 +6,7 @@ def rectangle(f,a,b,n):
         res=res+(b-a)*1.0/n*f(a+i*(b-a)*1.0/n)
     return res
 
-def f(x):
-    return x
+"""def f(x):
+    return x*x
 
-print(rectangle(f,0,1,1000))
+print(rectangle(f,0,1,1000))"""
